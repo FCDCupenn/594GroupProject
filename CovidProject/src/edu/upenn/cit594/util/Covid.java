b8e4106f8b9423669a0ec78bb8f3cf2507505e75
sunx68 <sunxin@seas.upenn.edu>
2023-04-15 12:13:51 -0700

    classes

--- CovidProject/src/edu/upenn/cit594/util/Covid.java
+++ CovidProject/src/edu/upenn/cit594/util/Covid.java
@@ -9,8 +9,76 @@ public class Covid {
 	private int deathToll;
 	private int hospitalization;
 	private int partialVaccinated;
-	private int fully_vaccinated;
+	private int fullyVaccinated;
 	private int booster;
 	private Date date;
 	private Date time;
+	
+	public Covid (int zipCode, int negTest, int posTest, int deathToll, int hospitalization, int partialVaccinated,
+			int fullyVaccinated, int booster,  Date date, Date time) {
+		
+		this.zipCode = zipCode;
+		this.negTest = negTest;
+		this.posTest = posTest;
+		this.deathToll = deathToll;
+		this.hospitalization = hospitalization;
+		this.partialVaccinated = partialVaccinated;
+		this.fullyVaccinated = fullyVaccinated;
+		this.booster = booster;
+		this.date = date;
+		this.time = time;
+		
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+
+	public int getNegTest() {
+		return negTest;
+	}
+
+
+	public int getPosTest() {
+		return posTest;
+	}
+
+
+	public int getDeathToll() {
+		return deathToll;
+	}
+
+
+	public int getHospitalization() {
+		return hospitalization;
+	}
+
+
+	public int getPartialVaccinated() {
+		return partialVaccinated;
+	}
+
+
+	public int getFullyVaccinated() {
+		return fullyVaccinated;
+	}
+
+
+	public int getBooster() {
+		return booster;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public Date getTime() {
+		return time;
+	}
+
+	
+	
 }
--- CovidProject/src/edu/upenn/cit594/util/Property.java
+++ CovidProject/src/edu/upenn/cit594/util/Property.java
@@ -5,5 +5,25 @@ public class Property {
 	private int marketValue;
 	private int totalLivableArea;
 	private int zipCode;
+	
+	public Property (int marketValue, int totalLivableArea, int zipCode) {
+		this.marketValue = marketValue;
+		this.totalLivableArea = totalLivableArea;
+		this.zipCode = zipCode;
+	}
+
+	public int getMarketValue() {
+		return marketValue;
+	}
+
+	public int getTotalLivableArea() {
+		return totalLivableArea;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+	
+	
 }
 
