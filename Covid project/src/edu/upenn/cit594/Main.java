24fd4d9fc0f67f503f6c519e3067badf700d3b02
kaiyinc168 <117884910+kaiyinc168@users.noreply.github.com>
2023-04-15 01:35:17 -0700

    Add files via upload

---
+++ Covid project/src/edu/upenn/cit594/Main.java
@@ -0,0 +1,15 @@
+package edu.upenn.cit594;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("testing");
+		
+		System.out.println("Hello again");
+
+
+	}
+
+}
