0eb3a4b36b3ccd32810b87b8db938fb2bc369d27
kaiyinc <kaiyinc168@gmail.com>
2023-04-15 19:30:15 -0700

    test

--- CovidProject/src/edu/upenn/cit594/datamanagement/CSVReader.java
+++ CovidProject/src/edu/upenn/cit594/datamanagement/CSVReader.java
@@ -1,60 +1,210 @@
 package edu.upenn.cit594.datamanagement;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import edu.upenn.cit594.util.State;
+import java.util.Stack;
 
 public class CSVReader {
-	
-	
-	/**This will read a file and return all state
-	 * 
-	 * @param fileName
-	 * @return return all state in a list
-	 */	
-public static List<State> readAllState(String fileName) {
-		
-		List<State> res = new ArrayList<State>();
-		// create a file
-		File file = new File(fileName);
-		// define a file reader
-		FileReader fileReader = null;
-		/// define buffered reader
-		BufferedReader bufferedReader = null;
-		try {
-			fileReader = new FileReader(file);
-			bufferedReader = new BufferedReader(fileReader);
-			String line;
-			
-			while ((line = bufferedReader.readLine()) != null) {
-				// split based on the tab
-				String[] lineArray = line.split(",");
-				String state = lineArray[0];
-				double latitude = Double.parseDouble(lineArray[1]);
-				double longtitude = Double.parseDouble(lineArray[2]);
-				State newState = new State(state, latitude, longtitude);
-				
-				res.add(newState);
 
-			}
-			// close the buffer reader after used it
-			bufferedReader.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Sorry, " + file.getName() + " not found.");
-			// e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    private HashMap<String, Integer> headerID;
+    private BufferedReader br;
+    private String filename;
+    private CharacterReader reader;
 
-		return res;
+    public CSVReader(CharacterReader reader) {
+        this.reader = reader;
+    }
 
-	}
+
+//    public CSVReader(String filename) throws IOException {
+//        this.filename = filename;
+//        this.br = new BufferedReader(new FileReader(filename));
+//        this.headerID = new HashMap<String, Integer>();
+//    }
+
+    public String[] readRow() throws IOException, CSVFormatException {
+        // check input stream one by one;
+        int c = this.reader.read();
+        StringBuilder str = new StringBuilder();
+        ArrayList<String> res = new ArrayList<>();
+        Stack<Integer> doubleQuote = new Stack<>();
+        String currentState = "Non_Escape";
+
+        // Switch state
+        while (c != -1) {
+            switch (currentState) {
+                case "Non_Escape":
+                    switch (c) {
+
+                        case 10: // line break LR
+                            // it should be the end of the line
+                            String s = str.toString();
+                            res.add(s);
+                            return res.toArray(new String[res.size()]);
+
+                        case 13: // CR
+
+
+                            // if there is a CR in the unescaped field, need to check if there is a LF
+                            c = reader.read();
+                            if (c == 10) {
+                                // there is a LF
+                                s = str.toString();
+                                res.add(s);
+                                return res.toArray(new String[res.size()]);
+                            } else
+                                throw new CSVFormatException();
+
+
+                        case 44: // comma
+                            s = str.toString();
+                            res.add(s);
+                            str = new StringBuilder();
+                            break;
+
+                        case 34: // Double Quote
+                            if (doubleQuote.isEmpty()) {
+                                doubleQuote.add(c);
+                            } else {
+                                doubleQuote.pop();
+                            }
+
+                            // the first double quote before anything
+                            if (str.length() == 0) {
+                                currentState = "Escape";
+                                // add the "" to the stack
+                                //doubleQuote.add(c);
+                            } else {
+                                throw new CSVFormatException();
+                            }
+                            break;
+
+                    }
+                    break;
+                case "Escape":
+                    switch (c) {
+                        case 44: // comma
+                            // in the escape state, the comma. need to add to the list
+                            str.append((char) c);
+                            break;
+
+                        case 34: // Double Quote
+                            // double quote need to appear in pairs
+                            // at the end of the program, the stack should be empty
+                            if (doubleQuote.isEmpty()) {
+                                doubleQuote.add(c);
+                            } else {
+                                doubleQuote.pop();
+                            }
+
+                            // this will be at least the second double quote
+
+                            // the middle double quote has to be in pairs
+                            c = reader.read();
+                            if (c == 34) {
+                                str.append((char) c);
+                                if (doubleQuote.isEmpty()) {
+                                    doubleQuote.add(c);
+                                } else {
+                                    doubleQuote.pop();
+                                }
+                            }
+                            // this " is the last one, and the next is ,
+                            else if (c == 44) {
+                                String s = str.toString();
+                                res.add(s);
+                                str = new StringBuilder();
+                                currentState = "Non_Escape";
+                            }
+                            // after " this is the CR,
+                            else if (c == 13) {
+
+                                //check if the next is LF
+                                c = reader.read();
+                                if (c == 10) {
+                                    String s = str.toString();
+                                    res.add(s);
+                                    return res.toArray(new String[res.size()]);
+                                }
+                                // if there is only 1 CR, throw an exception
+                                else {
+                                    throw new CSVFormatException();
+
+                                }
+                            }
+                            // this " is the last one, and the next is LR or -1
+                            else if (c == 10 || c == -1) {
+                                String s = str.toString();
+                                res.add(s);
+                                return res.toArray(new String[res.size()]);
+                            } else
+                                throw new CSVFormatException();
+
+
+                    }
+
+
+            }
+            // this step only add character other than "" and ,
+            if (c != 44 && c != 34) {
+                str.append((char) c); // cast c into the string
+                //System.out.println(str);
+            }
+
+
+            c = reader.read(); // read the next line;
+            // if the next line within while loop is -1
+            if (c == -1) {
+                if (!doubleQuote.isEmpty()) throw new CSVFormatException();
+                if (str != null) {
+                    String s = str.toString();
+                    res.add(s);
+                    return res.toArray(new String[res.size()]);
+                }
+            }
+
+
+        }
+        //  out of  while loop
+        // for the last sentence
+        if (!doubleQuote.isEmpty()) throw new CSVFormatException();
+
+        return null;
+
+    }
+
+    public HashMap<String, Integer> generateHeaderID() throws IOException, CSVFormatException {
+        String[] firstRow = readRow();
+        for(int i = 0; i<firstRow.length;i++){
+            String header = firstRow[i];
+            headerID.put(header, i);
+
+        }
+        return headerID;
+
+    }
+
+    public int getFieldPerHeader(String header){
+        int headerID = this.headerID.get(header);
+        return headerID;
+
+    }
+
+    public List<String[]> readAll() throws CSVFormatException, IOException {
+        List<String[]> allRows = new ArrayList<>();
+        String[] row;
+        while((row= readRow())!=null){
+            allRows.add(row);
+        }
+        return allRows;
+
+    }
+
+
+
+
 
 }
--- CovidProject/src/edu/upenn/cit594/datamanagement/CovidReader.java
+++ CovidProject/src/edu/upenn/cit594/datamanagement/CovidReader.java
@@ -1,11 +1,37 @@
 package edu.upenn.cit594.datamanagement;
 
+import edu.upenn.cit594.util.Covid;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class CovidReader {
 	
 	 public CSVReader csvReader;
 	 public JSONFileReader jReader;
+	 private Map<String, Integer> headerID;
 	 
-	 public CovidReader() {
-		 
-	 }
-}
+	 public CovidReader(String filename) throws IOException {
+		try( var reader = new CharacterReader(filename)){
+			var csvReader = new CSVReader(reader);
+			var allRows = csvReader.readAll();
+			headerID =csvReader.generateHeaderID();
+			List<Covid> covidDataList = new ArrayList<>();
+			for(var row:allRows){
+				String zipCode = row[headerID.get("zip_code")];
+				String timestamp = row[headerID.get("etl_timestamp")];
+				String partiallyVaccinated = row[headerID.get("partially_vaccinated")];
+				String fullyVaccinated = row[headerID.get("fully_vaccinated")];
+				Covid eachCovidRecord = new Covid(zipCode,timestamp,partiallyVaccinated,fullyVaccinated);
+				covidDataList.add(eachCovidRecord);
+			}
+			System.out.println(allRows.size());
+
+		} catch (CSVFormatException e) {
+			throw new RuntimeException(e);
+		}
+
+
+	 }}
